146dfe0cfe911d475657062af1ef142d19858b1b
spirits3 <flo_diablo@hotmail.com>
2019-03-27 11:27:30 +0100

    add function leibzig

--- main.cpp
+++ main.cpp
@@ -13,46 +13,64 @@
 
 #include <iostream>
 #include <cstdlib>
-#include "fraction.h"
+#include "Fraction.h"
 #include <limits>
 using namespace std;
 
 
+template <typename T>
+void LeibnizApproximation(const size_t& NB_ITERATIONS );
+
+
+int main(){
+
+    cout << "Fraction en int: " << endl;
+    LeibnizApproximation<int>(50);
+    cout<< endl << endl;
+
+    cout << "Fraction en long long: " << endl;
+    LeibnizApproximation<long long>(50);
+    cout << endl;
+    return EXIT_SUCCESS;
+}
+
+template<typename T>
+void test(const sizet& NB_ITERATIONS) {
+    try{
+    size_t i = 1;
+    int numerateur = 4;
+    int denumerateur = 1;
+    short signe = 1;
+    Fraction<T> f1(numerateur,denumerateur);
+
+    while(i < NB_ITERATIONS){
+        signe *= -1;
+        f1 += Fraction<T>((signe * numerateur),denumerateur+(2*i++)) ;
+        cout << "iteration numero " << i << " : " << (double)f1 << endl;
+    }
+
+    }catch( exception& e){
+        cout << e.what();
+    }
+
+}
+
+template<typename T>
 void LeibnizApproximation(const size_t& NB_ITERATIONS ){
     try{
         size_t i = 1;
         int numerateur = 4;
         int denumerateur = 1;
         short signe = 1;
-        Fraction<long long> f1(numerateur,denumerateur);
+        Fraction<T> f1(numerateur,denumerateur);
 
         while(i < NB_ITERATIONS){
             signe *= -1;
-            f1 += Fraction<long long>((signe * numerateur),denumerateur+(2*i++)) ;
-            cout << (double)f1 << endl;
+            f1 += Fraction<T>((signe * numerateur),denumerateur+(2*i++)) ;
+            cout << "iteration numero " << i << " : " << (double)f1 << endl;
         }
 
     }catch( exception& e){
         cout << e.what();
     }
-}
-
-int main(){
-
-    try{
-        size_t i = 1;
-        int numerateur = 4;
-        int denumerateur = 1;
-        short signe = 1;
-        Fraction<long long> f1(numerateur,denumerateur);
-
-        while(i < 50){
-            signe *= -1;
-            f1 += Fraction<long long>((signe * numerateur),denumerateur+(2*i++)) ;
-        }
-        //cout << f1 << endl;
-    }catch( exception& e){
-        cout << e.what();
-    }
-    return EXIT_SUCCESS;
 }
