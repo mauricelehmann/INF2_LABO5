ae1ee9e3961f6489d3da8582e7f0d7b0ce7b2a84
Maurice <m.lehmann94@gmail.com>
2019-03-27 11:41:54 +0100

    ça marche

--- main.cpp
+++ main.cpp
@@ -19,35 +19,66 @@ using namespace std;
 
 
 template <typename T>
-void LeibnizApproximation(const size_t& NB_ITERATIONS );
+void leibnizApproximation(const size_t& NB_ITERATIONS );
+template<typename T>
+void nilakanthaApproximation(const size_t& NB_ITERATIONS);
 
 
 int main(){
 
-    cout << "Fraction en int: " << endl;
-    LeibnizApproximation<int>(50);
+
+    const size_t NB_ITERATIONS = 50;
+
+    cout << "Approximation de Leibniz (int)" << endl;
+    leibnizApproximation<int>(NB_ITERATIONS);
     cout<< endl << endl;
 
-    cout << "Fraction en long long: " << endl;
-    LeibnizApproximation<long long>(50);
+    cout << "Approximation de Leibniz (long long)" << endl;
+    leibnizApproximation<long long>(NB_ITERATIONS);
+    cout << endl;
+
+    cout << "Meilleure approximation de Leibniz (int)" << endl;
+    nilakanthaApproximation<int>(NB_ITERATIONS);
+    cout<< endl << endl;
+
+    cout << "Meilleure approximation de Leibniz (long long)" << endl;
+    nilakanthaApproximation<long long>(NB_ITERATIONS);
     cout << endl;
     return EXIT_SUCCESS;
 }
 
 template<typename T>
-void LeibnizApproximation(const size_t& NB_ITERATIONS ){
+void leibnizApproximation(const size_t& NB_ITERATIONS ){
     try{
-        int numerateur = 4;
-        int denumerateur = 1;
+        T numerateur = 4;
+        T denumerateur = 1;
         short signe = 1;
         Fraction<T> f1(numerateur,denumerateur);
-
+        size_t i = 1;
         while(i < NB_ITERATIONS){
             signe *= -1;
             f1 += Fraction<T>((signe * numerateur),denumerateur+(2*i++)) ;
             cout << "iteration numero " << i << " : " << (double)f1 << endl;
         }
 
+    }catch( exception& e){
+        cout << e.what();
+    }
+}
+
+template<typename T>
+void nilakanthaApproximation(const size_t& NB_ITERATIONS){
+    try{
+        size_t i = 1;
+        T numerateur = 4;
+        T denumerateur = 2;
+        short signe = -1;
+        Fraction<T> f1(3,1);
+        while(i++ < NB_ITERATIONS){
+            signe *= -1;
+            f1 += Fraction<T>((signe * numerateur),(denumerateur++ * denumerateur++ * denumerateur)) ;
+            cout << "iteration numero " << i << " : " << (double)f1 << endl;
+        }
     }catch( exception& e){
         cout << e.what();
     }
